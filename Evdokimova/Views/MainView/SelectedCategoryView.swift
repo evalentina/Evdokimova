142a845803647d56ba146de0a630ab252e80f8b3
Evdokimova Valentina <evdokimovalentina00@gmail.com>
2023-07-02 15:58:55 +0300

    Add pinning of dish tags to SelectedCategoryView

--- Evdokimova/Views/MainView/SelectedCategoryView.swift
+++ Evdokimova/Views/MainView/SelectedCategoryView.swift
@@ -25,9 +25,8 @@ struct SelectedCategoryView<DetailModifier: ViewModifier>: View {
         
         ScrollView {
             
-            tagsOfDishes
-            
             dishes
+            
         }
         .task {
             
@@ -63,65 +62,74 @@ struct SelectedCategoryView<DetailModifier: ViewModifier>: View {
 
 extension SelectedCategoryView {
     
-    // MARK: Tags of dishes. When you click on the tag, the list of dishes is filtered
+    // MARK: Tags of dishes. When you click on the tag, the list of dishes is filtered. The tags are at the top while scrolling.
     
     var tagsOfDishes: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            LazyHGrid(rows: gridLayoutRows, spacing: 10) {
-                ForEach(Teg.allCases, id: \.self) { item in
-                    Button {
-                        viewModel.selectedTag = item
-                        viewModel.filterData()
-                    } label: {
-                        Text(item.rawValue)
-                            .textStyle(weight: .regular, size: 14)
-                            .padding(.horizontal, 16)
-                            .padding(.vertical, 10)
-                            .foregroundColor(viewModel.selectedTag == item ? .white : .black)
-                            .background(viewModel.selectedTag == item ? Color.blueColor : Color.buttonColor)
-                            .cornerRadius(10)
+        HStack {
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHGrid(rows: gridLayoutRows, spacing: 10) {
+                    ForEach(Teg.allCases, id: \.self) { item in
+                        Button {
+                            viewModel.selectedTag = item
+                            viewModel.filterData()
+                        } label: {
+                            Text(item.rawValue)
+                                .textStyle(weight: .regular, size: 14)
+                                .padding(.horizontal, 16)
+                                .padding(.vertical, 10)
+                                .foregroundColor(viewModel.selectedTag == item ? .white : .black)
+                                .background(viewModel.selectedTag == item ? Color.blueColor : Color.buttonColor)
+                                .cornerRadius(10)
+                        }
                     }
                 }
             }
-            .padding(.horizontal, 16)
         }
+        .padding(.vertical, 5)
+        .background(Color.white)
+        
     }
     
     // MARK: Grid for all dishes in the category
     
     var dishes: some View {
         
-        LazyVGrid(columns: gridLayoutColumns, alignment: .center, spacing: 8) {
-            ForEach(viewModel.filteredDishes) { dish in
-                Button {
-                    viewModel.openDishDetail(dish: dish)
-                } label : {
-                    VStack {
-                        
-                        GeometryReader { geo in
-                            // MARK: Geometry Reader modifies alignment, therefore, an additional stack is needed
+        LazyVGrid(columns: gridLayoutColumns, alignment: .center, spacing: 8, pinnedViews: [.sectionHeaders]) {
+            
+            //MARK: Makes tags pinned to the top
+            Section(header: tagsOfDishes) {
+                
+                ForEach(viewModel.filteredDishes) { dish in
+                    Button {
+                        viewModel.openDishDetail(dish: dish)
+                    } label : {
+                        VStack {
                             
-                            VStack(spacing: 0) {
-                                AsyncImageReusableView(imageURLString: dish.imageURL)
+                            GeometryReader { geo in
+                                // MARK: Geometry Reader modifies alignment, therefore, an additional stack is needed
+                                
+                                VStack(spacing: 0) {
+                                    AsyncImageReusableView(imageURLString: dish.imageURL)
+                                }
+                                .frame(width: geo.size.width,
+                                       height: geo.size.height,
+                                       alignment: .center)
                             }
-                            .frame(width: geo.size.width,
-                                   height: geo.size.height,
-                                   alignment: .center)
+                            .clipped()
+                            .aspectRatio(1, contentMode: .fit)
+                            .padding()
+                            .background(Color.buttonColor.cornerRadius(10))
+                            
+                            Text(dish.name)
+                                .textStyle(weight: .regular, size: 14)
+                                .foregroundColor(.black)
+                                .multilineTextAlignment(.leading)
+                                .frame(height: 35, alignment: .top)
                         }
-                        .clipped()
-                        .aspectRatio(1, contentMode: .fit)
-                        .padding()
-                        .background(Color.buttonColor.cornerRadius(10))
                         
-                        Text(dish.name)
-                            .textStyle(weight: .regular, size: 14)
-                            .foregroundColor(.black)
-                            .multilineTextAlignment(.leading)
-                            .frame(height: 35, alignment: .top)
                     }
-                    
+                    .modifier(detailModifier)
                 }
-                .modifier(detailModifier)
             }
         }
         .padding(.horizontal ,16)
